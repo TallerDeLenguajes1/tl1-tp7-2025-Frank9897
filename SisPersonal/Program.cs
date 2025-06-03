27076c056891d7f08b9bb44cdeac1127970e211b
Frank9897 <franco.vasquez9897@gmail.com>
2025-06-02 21:52:00 -0300

    Codificacion Apartado e)

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,32 +1,58 @@
 ﻿using System.Globalization;
 using esEmpleado;
 
-Console.WriteLine("Ingrese nombre de empleado");
-string? nombre = Console.ReadLine();
-Console.WriteLine($"Ingrese apellido de {nombre}");
-string? apellido = Console.ReadLine();
-Console.WriteLine($"Ingrese fecha de nacimiento de {apellido} {nombre}");
-DateTime fechaDeNacimiento = DateTime.Parse(Console.ReadLine());
-Console.WriteLine($"Ingrese estado civil de {apellido} {nombre}");
-char estadocivil = char.Parse(Console.ReadLine());
-Console.WriteLine($"Ingrese fecha de ingreso de {apellido} {nombre}");
-DateTime fechaingreso = DateTime.Parse(Console.ReadLine());
-Console.WriteLine($"Ingrese sueldo basico de {apellido} {nombre}");
-double basico = double.Parse(Console.ReadLine());
+Empleado[] empleados = new Empleado[3];
 
-Console.WriteLine("Seleccione cargo:");
-foreach (var cargo in Enum.GetValues(typeof(Cargos)))
+for (int i = 0; i < empleados.Length; i++)
 {
-    Console.WriteLine($"{(int)cargo + 1} - {cargo}");
+    Console.WriteLine($"Ingrese los datos del empleado N°{i+1}");
+    Console.WriteLine("Ingrese nombre de empleado");
+    string? nombre = Console.ReadLine();
+    Console.WriteLine($"Ingrese apellido de {nombre}");
+    string? apellido = Console.ReadLine();
+    Console.WriteLine($"Ingrese fecha de nacimiento de {apellido} {nombre}");
+    DateTime fechaDeNacimiento = DateTime.Parse(Console.ReadLine());
+    Console.WriteLine($"Ingrese estado civil de {apellido} {nombre}");
+    char estadocivil = char.Parse(Console.ReadLine());
+    Console.WriteLine($"Ingrese fecha de ingreso de {apellido} {nombre}");
+    DateTime fechaingreso = DateTime.Parse(Console.ReadLine());
+    Console.WriteLine($"Ingrese sueldo basico de {apellido} {nombre}");
+    double basico = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Seleccione cargo:");
+    foreach (var cargo in Enum.GetValues(typeof(Cargos)))
+    {
+        Console.WriteLine($"{(int)cargo + 1} - {cargo}");
+    }
+    int cargoSeleccionado = int.Parse(Console.ReadLine());
+    Cargos cargoEnum = (Cargos)(cargoSeleccionado-1);
+
+    Empleado nuevo = new Empleado(nombre, apellido, fechaDeNacimiento, estadocivil, fechaingreso, basico, cargoEnum);
+
+    nuevo.calcAntiguedad(fechaingreso);
+    nuevo.calcEdad(fechaDeNacimiento);
+    nuevo.calcJubilacion();
+    nuevo.calcSalario(basico, nuevo.Antiguedad, estadocivil, cargoEnum);
+
+    empleados[i] = nuevo;
+
 }
-int cargoSeleccionado = int.Parse(Console.ReadLine());
-Cargos cargoEnum = (Cargos)(cargoSeleccionado-1);
 
-Empleado nuevo = new Empleado(nombre, apellido, fechaDeNacimiento, estadocivil, fechaingreso, basico, cargoEnum);
+double total = 0;
+foreach (var empleado in empleados)
+{
+    total += empleado.Salario;
+}
 
-nuevo.calcAntiguedad(fechaingreso);
-nuevo.calcEdad(fechaDeNacimiento);
-nuevo.calcJubilacion();
-nuevo.calcSalario(basico, nuevo.Antiguedad, estadocivil, cargoEnum);
+Console.WriteLine($"Total de monto a pagar empleados: ${total}");
 
-Console.WriteLine($"EMPLEADO CARGADO\nNombre: {nuevo.Nombre}\nApellido: {nuevo.Apellido}\n Edad: {nuevo.Edad}\nFecha de nacimiento: {nuevo.FechaDeNacimiento:dd/MM/yyyy}\nSalario NETO: ${nuevo.Salario}");
+Empleado jubilacionproxima = empleados[0];
+
+foreach (var empleado in empleados)
+{
+    if (empleado.Jubilacion < jubilacionproxima.Jubilacion)
+    {
+        jubilacionproxima = empleado;
+    }
+}
+Console.WriteLine($"Empleado mas proximo a Jubilarse\nNombre: {jubilacionproxima.Nombre}\nApellido: {jubilacionproxima.Apellido}\n Edad: {jubilacionproxima.Edad} Anios\nFecha de nacimiento: {jubilacionproxima.FechaDeNacimiento:dd/MM/yyyy}\nAntiguedad: {jubilacionproxima.Antiguedad} Anios\nSalario NETO: ${jubilacionproxima.Salario}");
